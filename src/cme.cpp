1a9315ea9405b8f0bc2a3c89c9df2a2959727123
Kexin Xie <kexinx@vt.edu>
2024-05-23 15:37:11 -0400

    initial mg

--- src/cme.cpp
+++ src/cme.cpp
@@ -1288,12 +1288,13 @@ List cme_gaussian(NumericMatrix& XX, NumericVector& yy,
       fill(delta.begin(),delta.end(),lambda);
       for (int g=0; g<J; g++) {
 
+        delta[g] = delta[g] * mg[g];
         cur_lambda = lambda*mg[g];
          //assigns each element the value lambda
 
         for (int j=K1[g];j<K1[g+1];j++){
           //int gind = floor((double)g/2.0); //index for sibling or cousin group
-          delta[g] = delta[g] * mg[g] * (exp(-(tau/cur_lambda) * mcp(beta[j],cur_lambda,gamma) )) ;
+          delta[g] = delta[g] * (exp(-(tau/cur_lambda) * mcp(beta[j],cur_lambda,gamma) )) ;
         }
       }
 
